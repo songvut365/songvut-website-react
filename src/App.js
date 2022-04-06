4b9020612d1a94450eaae04d0e1c0e276d780384
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-04-07 00:38:46 +0700

    Add app bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Home from './Home/Home';
+import ResponsiveAppBar from './components/AppBar'
+
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
@@ -7,6 +9,8 @@ import Container from '@mui/material/Container';
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar />
+
       <Container maxWidth="lg">
         <Box sx={{pt: 5, px: 5, bgcolor: 'white'}}>
           <Home/>
