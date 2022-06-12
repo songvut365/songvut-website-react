8668b83e9dc98a7e0ee3db99157e29d95951c255
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-12 19:12:55 +0700

    Create information components (left section)

---
+++ src/assets/data.js
@@ -0,0 +1,12 @@
+export const information = {
+  name: "songvut nakrong",
+  position: "web developer",
+  summary: "Hi! I'm 24 years old, A fresh graduate seeking a full-time position in the field of web development, Where I can apply my knowledge and skills for continuous improvement.",
+  email: "songvut.nakrong@gmail.com",
+  phone: "096-615-3710",
+  address: "Bangkok, Thailand",
+  githubLink: "https://www.github.com/songvut365",
+  linkedInLink: "https://www.linkedin.com/in/songvut-nakrong/",
+  facebookLink: "https://www.facebook.com/songvut365/",
+  instagramLink: "https://www.instagram.com/songvut.365/",
+}
