e0201f8066dfb626002f58916132dddd0ae1be9b
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-12 21:27:59 +0700

    Update skill detail and image

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ import { skills } from '../assets/data'
 export default function Skills() {
   return (
     <div>
-      <h1 className='text-3xl font-semibold text-blue-500'>Skills</h1>
+      <h1 className='text-3xl font-semibold text-blue-500'>SKILLS</h1>
 
       <div className="hs-accordion-group my-4" data-hs-accordion-always-open>
         { skills.map(skill => (
@@ -14,15 +14,32 @@ export default function Skills() {
               {skill.name}
             </button>
 
-            <div id="hs-basic-bordered-collapse-two" className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300" aria-labelledby="hs-bordered-heading-two">
-              <div className="pb-4 px-5">
-                {skill.details.map(detail=> (
-                  <div>
-                    <p>{detail.name}</p>
-                  </div>
-                ))}
+            {skill.name === "Language Proficiency" ? (
+              <div id="hs-basic-bordered-collapse-two" className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300" aria-labelledby="hs-bordered-heading-two">
+                <div className="pb-4 px-5 flex text-center justify-center flex-wrap gap-8 box-border">
+                  {skill.details.map(detail=> (
+                    <div key={detail.name}>
+                      <span className={"mb-1 inline-flex items-center justify-center h-[3rem] w-[3rem] " + detail.color}>
+                        <span className="text-lg font-medium text-white leading-none">{detail.short}</span>
+                      </span>
+                      <p className='text-sm font-semibold'>{detail.name} : {detail.level}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
+            ) : (
+              <div id="hs-basic-bordered-collapse-two" className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300" aria-labelledby="hs-bordered-heading-two">
+                <div className="pb-4 px-5 flex text-center justify-center flex-wrap gap-8 box-border">
+                  {skill.details.map(detail=> (
+                    <div key={detail.name}>
+                      <img src={detail.image} className="h-16" />
+                      <p className='text-sm font-semibold'>{detail.name}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+            
 
           </div>
         ))}
