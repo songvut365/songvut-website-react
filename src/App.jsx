d0f5c925d6814e2ba114d2ee0386a8528da331cc
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-06-13 18:44:58 +0700

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ function App() {
   return (
     <div className="bg-slate-900 text-slate-200 p-4">
       <Home />
+
+      <div className='absolute left-0 text-center font-semibold w-full mt-4 py-2 bg-slate-800'>
+        <p>Copyright © 2022 All rights reserved.</p>
+      </div>
     </div>
   )
 }
