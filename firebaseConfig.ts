687ea6789b19e45e1d8e6e3b240b9b6974aaba2d
songvut365 <songvut.nakrong@gmail.com>
2024-01-13 18:34:09 +0700

    add firebaseConfig.ts

---
+++ firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "songvut365.firebaseapp.com",
+  projectId: "songvut365",
+  storageBucket: "songvut365.appspot.com",
+  messagingSenderId: process.env.FIREASE_MESSAGEING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
