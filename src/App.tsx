490ca254f51c0860181ada173fa12fbe4a08b3f9
songvut365 <songvut.nakrong@gmail.com>
2024-01-13 23:27:49 +0700

    feat: understruction page

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,15 @@ import Footer from "./components/Footer";
 const App: React.FC = () => {
   return (
     <RecoilRoot>
-      <div className="App bg-gradient-to-br from-zinc-900 to-neutral-800 bg-fixed text-zinc-300 p-4">
+      {/* <div className="App bg-gradient-to-br from-zinc-900 to-neutral-800 bg-fixed text-zinc-300 p-8">
         <Home />
         <Footer />
+      </div> */}
+      <div className="bg-zinc-800 w-full h-screen flex flex-col justify-center items-center">
+        <h1 className="text-9xl text-yellow-400">UNDER CONSTRUCTION</h1>
       </div>
     </RecoilRoot>
   );
-}
+};
 
 export default App;
